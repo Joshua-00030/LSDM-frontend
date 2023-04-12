a27bbed31dd3fa9ebe14c748baecdc52f4a009e1
Joshua Romero <joshua.romero@my.utsa.edu>
2023-04-12 17:43:32 -0500

    start of the project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+//import './Navbar.css'
+import { useEffect, useState } from 'react';
+import { IconContext } from "react-icons";
+import { FaSearch } from 'react-icons/fa';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const Navbar = () => {
+    const [searchQuery, setSearchQuery] = useState("")
+    const [sortType, setSortType] = useState("0")
+
+    return(
+        <div>
+            <p>hello</p>
+        
+        </div>
+    )
+
+}
+export default Navbar
+
